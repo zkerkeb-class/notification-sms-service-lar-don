fb32a4f259e66679e53cf0a37b49fd9f0d9b5cfd
RyanPeyrot <ryan.peyrot@gmail.com>
2024-04-25 18:31:34 +0200

    add Metrics

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const apiRouter = require('./routes/index');
+const {webMetrics} = require("./utils/webMetrics");
 require('dotenv').config();
 
 // Middleware pour parser le corps des requêtes JSON
@@ -9,6 +10,7 @@ app.use(apiRouter)
 app.get('/confirm', (req, res) => {
     res.send('Bonjour, le serveur Express est en fonctionnement !');
 });
+app.get('/metrics',webMetrics);
 // Démarrer le serveur
 const PORT = process.env.PORT
 app.listen(PORT, () => {
