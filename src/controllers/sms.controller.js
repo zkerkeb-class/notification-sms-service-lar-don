605644f62bd73e83fc94c413e5f1f0fa511cdefe
Aubin Olivrie <aubin.olivrie@smice.com>
2024-05-04 23:06:28 +0200

    [ADD] Password change with SMS code

--- src/controllers/sms.controller.js
+++ src/controllers/sms.controller.js
@@ -1,23 +1,24 @@
 require('dotenv').config();
-const { Vonage } = require('@vonage/server-sdk')
+const { Vonage } = require('@vonage/server-sdk');
 
 const vonage = new Vonage({
-    apiKey: process.env.VONAGE_API_KEY,
-    apiSecret: process.env.VONAGE_API_SECRET
-})
+  apiKey: process.env.VONAGE_API_KEY,
+  apiSecret: process.env.VONAGE_API_SECRET,
+});
 
 exports.sendSms = async (req, res) => {
-    const from = "Lardon Services"
-    const to = req.body.phoneNumber
-    const text = req.body.smsContent
+  const from = 'Lardon Services';
+  const to = req.body.phoneNumber;
+  const text = req.body.smsContent;
 
-    await vonage.sms.send({to, from, text})
-        .then(resp => {
-            console.info(resp);
-            return res.send('Message sent successfully');
-        })
-        .catch(err => {
-            console.error(err);
-            res.status(500).send('There was an error sending the messages.');
-        });
-}
+  await vonage.sms
+    .send({ to, from, text })
+    .then((resp) => {
+      console.info(resp);
+      return res.send('Message sent successfully');
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('There was an error sending the messages.');
+    });
+};
