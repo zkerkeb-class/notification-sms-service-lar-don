69d5cecb136a80c86388d624201baa2f62f57ed0
Aubin Olivrie <aubin.olivrie@smice.com>
2024-05-04 20:17:04 +0200

    [ADD] Email verification

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 const express = require('express');
 const app = express();
 const apiRouter = require('./routes/index');
+const bodyParser = require('body-parser');
 require('dotenv').config();
 
-// Middleware pour parser le corps des requêtes JSON
 app.use(express.json());
-app.use(apiRouter)
-app.get('/confirm', (req, res) => {
-    res.send('Bonjour, le serveur Express est en fonctionnement !');
-});
-// Démarrer le serveur
-const PORT = process.env.PORT
-app.listen(PORT, () => {
-    console.info(`Serveur démarré sur le port ${PORT}`);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(apiRouter);
+
+app.listen(process.env.PORT, () => {
+  console.info(`[NOTIFS] Serveur démarré sur le port ${process.env.PORT}`);
 });
